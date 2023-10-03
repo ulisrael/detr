01fef6ed96a47938dea645ca5cdbc19ef1ffbbf2
ulisrael <ulisrael@caltech.edu>
2023-10-03 11:56:48 -0700

    new ann files

--- datasets/coco.py
+++ datasets/coco.py
@@ -153,8 +153,8 @@ def build(image_set, args):
     #     "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
     # }
     PATHS = {
-        "train": (root / "train", root / "train" / "_annotations.coco.json"),
-        "val": (root / "valid", root / "valid" / "_annotations.coco.json"),
+        "train": (root / "train", root / "annotations" / f'train_new.json'),
+        "val": (root / "val", root / "annotations" / f'val_new.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
